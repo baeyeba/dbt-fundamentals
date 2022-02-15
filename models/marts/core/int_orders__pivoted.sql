d4263bf1ea1cdca93e479c53a88d8c66e1c44885
bart.baeyens@intodata.eu <bart.baeyens@intodata.eu>
2022-02-15 11:10:23 +0000

    Nog een poging

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -4,12 +4,12 @@ with orders as (
 ),
 pivoted as (
     select customer_id,
-        {%- for status in status_values %}
+        {% for status in status_values %}
             sum(case when status = '{{ status }}' then 1 else 0 end) as {{status}}_aantal
             {%- if not loop.last -%}
                 ,
             {%- endif %}
-        {%- endfor -%}
+        {%- endfor %}
     from orders
     group by customer_id
     order by customer_id
